d2758a6f129f811f14a3fe06c77f0a9832ecaa7e
Scott Cruwys <scruwys@gmail.com>
2017-02-16 19:02:38 -0600

    Added functions for films extraction

--- extract/films.py
+++ extract/films.py
@@ -1,15 +1,38 @@
+#
+# 
+#
 import re
-import csv
 import util
-import requests
-from bs4 import BeautifulSoup
+
+def parse_rotten_tomatoes():
+	""" Attributes: Audience Score, Critic Score (# of ratings?) """
+	return False
+
+
+def parse_imdb():
+	""" TBD. """
+	return False
+
+
+def parse_box_office_mojo():
+	""" Attributes: Domestic Gross, Foreign Gross, Worldwide Gross """
+	return False
+
+
+def parse_wikipedia():
+	""" Attributes: Running time, """
+	return False
+
 
 def extract(opts):
     response = util.retrieve_clean_response(opts['href'])
+    infobox = response.find('table', {'class': 'infobox'})
 
-    print response
+    # for item in infobox.find_all('th'):
+    # 	print item.text.strip()
 
+    for item in response.find_all('a', {'class': 'external'}):
+    	print item
 
 if __name__ == '__main__':
-    inputs = {'category': 'Supporting Actor', 'name': 'Edward Norton', 'winner': '0', 'award': 'Guild', 'href': 'https://en.wikipedia.org/wiki/Birdman_(film)', 'year': '2014', 'film': 'Birdman'}
-    extract(inputs)
+    print "These are not the droids you're looking for..."
